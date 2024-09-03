80ea7840198bb390519f66fe346b836f3cc9bb4b
Ross Hume - Admin <Ross-Admin@peakdistrict.gov.uk>
2024-09-03 12:58:49 +0100

    Updated index with pdnpa refs

--- reportconfig.js
+++ reportconfig.js
@@ -1,16 +1,21 @@
-const reportConfig =
+const reportConfig = 
 {
-	"title": "NPMP Consultation Results 2021",
-	"themeConfig": {
-		"showSidebarHomepage": false,
-		"lastUpdated": false,
-		"sidebar": [
-			{
-				"title": "Home",
-				"path": "/"
-			}
-		]
-	}
+    "title": "NPMP Consultation Results 2021",
+    "themeConfig": {
+        "showSidebarHomepage": false,
+        "lastUpdated": false,
+        "sidebar": [
+            {
+                "title": "Home",
+                "path": "/",
+                "text": "Home",
+                "type": "html",
+                "pdnpa_id": "0f8eaeae-dfa8-47c9-b06c-18e7a11df5b6",
+                "pdnpa_parent": 0,
+                "pdnpa_is_folder": 0
+            }
+        ]
+    }
 }
 
 exports.reportConfig = reportConfig
